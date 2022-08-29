1ab7f6ad4c83bdbb7150c1f763af8c2009aa2b26
Hans Georg Krauthäuser <hgk@ieee.org>
2022-08-29 13:21:00 +0200

    new pyvisa

--- LargeRC/script/sw_rc_tx.py
+++ LargeRC/script/sw_rc_tx.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import visa
+# import pyvisa
 
 from mpy.device.driver import DRIVER
 from mpy.tools.Configuration import strbool
@@ -8,14 +8,14 @@ from mpy.tools.util import format_block
 
 
 class SWController(DRIVER):
-    conftmpl={'description': 
-                 {'description': str,
-                  'type': str,
-                  'vendor': str,
-                  'serialnr': str,
-                  'deviceid': str,
-                  'driver': str,
-                  'class': str},
+    conftmpl = {'description':
+                    {'description': str,
+                     'type': str,
+                     'vendor': str,
+                     'serialnr': str,
+                     'deviceid': str,
+                     'driver': str,
+                     'class': str},
                 'init_value':
                     {'fstart': float,
                      'fstop': float,
@@ -26,59 +26,60 @@ class SWController(DRIVER):
                      'virtual': strbool}}
 
     def __init__(self):
-        self.LF='R1P1'
-        self.HF='R1P2'
-        self.term_chars = visa.LF
+        self.LF = 'R1P1'
+        self.HF = 'R1P2'
+        self.term_chars = '\n'  # visa.LF
         DRIVER.__init__(self)
-        self.error=0
-        self.islf=None
-    
+        self.error = 0
+        self.islf = None
+
     def ask(self, cmd):
         if not self.dev:
             return None
-        ans=self.dev.ask(cmd)
+        ans = self.dev.ask(cmd)
         return ans
-    
-    def Init(self, ini, ch=1):
-        self.error=DRIVER.Init(self, ini, ch)
 
-        self.out='term'
+    def Init(self, ini, ch=1):
+        self.error = DRIVER.Init(self, ini, ch)
+
+        self.out = 'term'
         try:
             self.out = self.conf['init_value']['output']
-            self.out = fstrcmp(self.out,('term','rc'), n=1, cutoff=0, ignorecase=True)[0]
+            self.out = fstrcmp(self.out, ('term', 'rc'), n=1, cutoff=0, ignorecase=True)[0]
         except KeyError:
             pass
         if self.out == 'rc':
-            self.LF='R1P1R2P1'
-            self.HF='R1P2R2P2'
-        else:    
-            self.LF='R1P1R2P0'
-            self.HF='R1P2R2P0'
-        
-        self.swfreq=1e9
+            self.LF = 'R1P1R2P1'
+            self.HF = 'R1P2R2P2'
+        else:
+            self.LF = 'R1P1R2P0'
+            self.HF = 'R1P2R2P0'
+
+        self.swfreq = 1e9
         try:
             self.swfreq = self.conf['init_value']['swfreq']
         except KeyError:
             pass
         return self.error
-        
+
     def SetFreq(self, f):
-        if f<=self.swfreq:
-            cmd=self.LF
+        if f <= self.swfreq:
+            cmd = self.LF
         else:
-            cmd=self.HF
-        ans=self.ask(cmd)
+            cmd = self.HF
+        ans = self.ask(cmd)
         return 0, f
-        
+
     def Quit(self):
         return 0
+
 
 if __name__ == '__main__':
     import io
     import numpy as np
     import time
 
-    ini=format_block("""
+    ini = format_block("""
                     [DESCRIPTION]
                     DESCRIPTION = RC TX Switch
                     TYPE = Custom
@@ -97,11 +98,11 @@ if __name__ == '__main__':
                     SWFREQ = 1e9
                     VIRTUAL = 0
                     """)
-    ini=io.StringIO(ini)
-    
-    sw=SWController()
+    ini = io.StringIO(ini)
+
+    sw = SWController()
     sw.Init(ini)
-    for f in np.linspace(0,4.2e9,10):
+    for f in np.linspace(0, 4.2e9, 10):
         print(f, sw.SetFreq(f))
         time.sleep(1)
 
